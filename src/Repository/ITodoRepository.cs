f318f09b5302d0a23933a32ff0b95759fe860d1d
Kristian Enge <kristian.enge@ikomm.no>
2021-02-18 11:18:20 +0100

    ItodoRepo

---
+++ src/Repository/ITodoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoAPI.Models;
+
+namespace TodoAPI.Repository
+{
+    public interface ITodoRepository
+    {
+        Tuple<bool, Todo> Add(Todo todo);
+        bool Delete(int id);
+        IEnumerable<Todo> GetAll();
+        Todo GetById(int id);
+        bool Update(int id, Todo todo);
+    }
+}
